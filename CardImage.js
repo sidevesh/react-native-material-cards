e876f3604ce9d85df131fca05b283017e031f273
Sven <sho1975@gmail.com>
2017-12-11 21:52:55 +0100

    Fix Error: <Image> component cannot contain children.
    Trying to move the text outside of the image.

--- CardImage.js
+++ CardImage.js
@@ -3,7 +3,7 @@ import {
   StyleSheet,
   Text,
   View,
-  Image
+  ImageBackground
 } from 'react-native';
 
 export default class CardImage extends Component {
@@ -17,11 +17,11 @@ export default class CardImage extends Component {
     const newStyle = this.props.style || {};
     return (
       <View style={[styles.cardImage, newStyle]} onLayout={(e)=>{this.setState({calc_height: e.nativeEvent.layout.width*9/16});}}>
-        <Image source={this.props.source} resizeMode={this.props.resizeMode || "stretch"} resizeMethod={this.props.resizeMethod || "resize"} style={[styles.imageContainer,  {height: this.state.calc_height}]}>
+        <ImageBackground source={this.props.source} resizeMode={this.props.resizeMode || "stretch"} resizeMethod={this.props.resizeMethod || "resize"} style={[styles.imageContainer,  {height: this.state.calc_height}]}>
           {this.props.title!==undefined &&
             <Text style={styles.imageTitleText}>{this.props.title}</Text>
           }
-        </Image>
+          </ImageBackground>
       </View>
     );
   }
