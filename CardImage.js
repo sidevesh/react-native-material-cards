d6d8eb903e8e8851648d6ac0eeae0057846657ed
Kittinutz <5635512014@email.psu.ac.th>
2018-03-26 01:41:30 +0700

    fix Image to ImagebackGround

--- CardImage.js
+++ CardImage.js
@@ -1,53 +1,59 @@
 import React, {Component} from 'react';
 import {
-  StyleSheet,
-  Text,
-  View,
-  Image
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    ImageBackground
 } from 'react-native';
 
 export default class CardImage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      calc_height: 0
+    constructor(props) {
+        super(props);
+        this.state = {
+            calc_height: 0
+        }
     }
-  }
-  render () {
-    const newStyle = this.props.style || {};
-    return (
-      <View style={[styles.cardImage, newStyle]} onLayout={(e)=>{this.setState({calc_height: e.nativeEvent.layout.width*9/16});}}>
-        <Image source={this.props.source} resizeMode={this.props.resizeMode || "stretch"} resizeMethod={this.props.resizeMethod || "resize"} style={[styles.imageContainer,  {height: this.state.calc_height}]}>
-          {this.props.title!==undefined &&
-            <Text style={styles.imageTitleText}>{this.props.title}</Text>
-          }
-        </Image>
-      </View>
-    );
-  }
+
+    render() {
+        const newStyle = this.props.style || {};
+        return (
+            <View style={[styles.cardImage, newStyle]} onLayout={(e) => {
+                this.setState({calc_height: e.nativeEvent.layout.width * 9 / 16});
+            }}>
+                <ImageBackground source={this.props.source} resizeMode={this.props.resizeMode}
+                                 resizeMethod={this.props.resizeMethod}
+                                 style={[styles.imageContainer, {height: this.state.calc_height}]}>
+                    {this.props.title !== undefined &&
+                    <Text style={styles.imageTitleText}>{this.props.title}</Text>
+                    }
+                </ImageBackground>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  cardImage: {
-    flex: 1,
-    flexDirection: 'row',
-    backgroundColor: 'grey',
-    alignSelf: 'stretch',
-    marginBottom: 16,
-    justifyContent: 'center',
-    alignItems: 'stretch'
-  },
-  imageContainer: {
-    flex: 1,
-    flexDirection: 'column',
-    paddingRight: 16,
-    paddingLeft: 16,
-    paddingBottom: 16,
-    paddingTop: 16,
-    justifyContent: 'flex-end'
-  },
-  imageTitleText: {
-    fontSize: 24,
-    color: 'rgba(255 ,255 ,255 , 0.87)'
-  }
+    cardImage: {
+        flex: 1,
+        flexDirection: 'row',
+        backgroundColor: 'grey',
+        alignSelf: 'stretch',
+        marginBottom: 16,
+        justifyContent: 'center',
+        alignItems: 'stretch'
+    },
+    imageContainer: {
+        flex: 1,
+        flexDirection: 'column',
+        paddingRight: 16,
+        paddingLeft: 16,
+        paddingBottom: 16,
+        paddingTop: 16,
+        justifyContent: 'flex-end'
+    },
+    imageTitleText: {
+        fontSize: 24,
+        color: 'rgba(255 ,255 ,255 , 0.87)'
+    }
 });
