63f7ae4225faebbda1e31d7c8b2efbed6c6fb140
Matthias Seiffert <lythenas@hotmail.de>
2019-02-09 17:36:08 +0100

    Added feature to replace the image component of CardImage

--- CardImage.js
+++ CardImage.js
@@ -3,29 +3,36 @@ import {
   StyleSheet,
   Text,
   View,
-  ImageBackground
+  Image
 } from 'react-native';
 
 export default class CardImage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      calc_height: 0
+      calc_height: 0,
+      componentWidth: 350,
+      componentHeight: 194
     }
   }
   render () {
     const newStyle = this.props.style || {};
+    const Component = this.props.component || Image;
+
     return (
       <View style={[styles.cardImage, newStyle]} onLayout={(e)=>{this.setState({calc_height: e.nativeEvent.layout.width*9/16});}}>
 
-        <ImageBackground source={this.props.source} resizeMode={this.props.resizeMode || "stretch"} resizeMethod={this.props.resizeMethod || "resize"} style={[styles.imageContainer,  {height: this.state.calc_height}]}>
+        <View style={[styles.imageContainer, {height: this.props.imageHeight || this.state.calc_height}]} onLayout={(e) => {this.setState({componentHeight: e.nativeEvent.layout.height, componentWidth: e.nativeEvent.layout.width});}}>
+          <Component source={this.props.source} resizeMethod={this.props.resizeMethod || "resize"} style={[StyleSheet.absoluteFill, { width: this.props.imageWidth || this.state.componentWidth, height: this.props.imageHeight || this.state.componentHeight }, this.props.imageStyle]} />
+        </View>
+        <View styles={styles.imageContainer}>
           {this.props.title!==undefined && this.props.singleLineTitle == true &&
             <Text numberOfLines={1} style={styles.imageTitleText}>{this.props.title}</Text>
           }
           {this.props.title!==undefined && (this.props.singleLineTitle == false || this.props.singleLineTitle === undefined) &&
             <Text style={styles.imageTitleText}>{this.props.title}</Text>
           }
-          </ImageBackground>
+        </View>
 
       </View>
     );
@@ -40,16 +47,21 @@ const styles = StyleSheet.create({
     alignSelf: 'stretch',
     marginBottom: 16,
     justifyContent: 'center',
-    alignItems: 'stretch'
+    alignItems: 'stretch',
+    overflow: "hidden"
   },
   imageContainer: {
+    position: "absolute",
+    top: 0,
+    left: 0,
     flex: 1,
     flexDirection: 'column',
     paddingRight: 16,
     paddingLeft: 16,
     paddingBottom: 16,
     paddingTop: 16,
-    justifyContent: 'flex-end'
+    justifyContent: 'flex-end',
+    width: "100%"
   },
   imageTitleText: {
     fontSize: 24,
